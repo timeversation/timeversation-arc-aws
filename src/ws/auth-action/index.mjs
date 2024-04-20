06ae2e02e6ea454f40811f5f9f684f4636be2364
wonglok <yellowhappy831@gmail.com>
2024-04-21 05:31:09 +0800

    index.mjs

--- src/ws/auth-action/index.mjs
+++ src/ws/auth-action/index.mjs
@@ -1,5 +1,7 @@
 // learn more about WebSocket functions here: https://arc.codes/ws
 export async function handler(req) {
+
   console.log(JSON.stringify(req, null, 2))
+
   return { statusCode: 200 }
 }
