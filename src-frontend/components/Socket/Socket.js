3d9acc68966d7ba7ae8f37b4bd3b3278cf30be60
wonglok <yellowhappy831@gmail.com>
2024-04-21 05:07:39 +0800

    save

--- src-frontend/components/Socket/Socket.js
+++ src-frontend/components/Socket/Socket.js
@@ -2,12 +2,44 @@ import React, { useEffect } from "react"
 import ReconnectingWebSocket from 'reconnecting-websocket'
 
 export function SocketApp() {
+    let [socket, setSocket] = React.useState(null)
     useEffect(() => {
         // This is a JavaScript module
-        let socket = new ReconnectingWebSocket(window.BACKEND_RESOURCES.socket)
-        socket.onopen = function (e) {
+        let socketinst = new ReconnectingWebSocket(window.BACKEND_RESOURCES.socket)
+        socketinst.onopen = function (e) {
             console.log('[open] Connection established')
             console.log('Sending to server')
+        }
+
+        socketinst.onmessage = function ({ data: rawJSON }) {
+            let data = JSON.parse(rawJSON)
+            console.log(data)
+        }
+
+        setSocket(socketinst)
+
+        return () => {
+            socketinst.close()
+        }
+    }, [])
+    return <>
+        <button className="bg-gray-200 p-3 px-5 mr-3" onClick={() => {
+            fetch(window.BACKEND_RESOURCES.rest + '/auth', {
+                method: 'POST',
+                mode: 'cors',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ client: 'source from desktop' })
+            }).then(r => {
+                return r.json()
+            }).then(v => {
+                console.log(v)
+            })
+        }}>Autnentication</button>
+
+        <button className="bg-gray-200 p-3 px-5 mr-3" onClick={() => {
+            //
 
             socket.send(JSON.stringify({
                 action: 'walk-action',
@@ -15,38 +47,8 @@ export function SocketApp() {
                     happy: 123
                 }
             }))
-        }
 
-        window.addEventListener('falala', ({ detail }) => {
-            console.log(detail)
-        })
-
-
-        // window.addEventListener('')
-
-        socket.onmessage = function ({ data: rawJSON }) {
-            let data = JSON.parse(rawJSON)
-            console.log(data)
-        }
-
-        fetch(window.BACKEND_RESOURCES.rest + '/auth', {
-            method: 'POST',
-            mode: 'cors',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ client: 'source from desktop' })
-        }).then(r => {
-            return r.json()
-        }).then(v => {
-            console.log(v)
-        })
-
-
-    })
-    return <>
-        <button onClick={() => {
-            window.dispatchEvent(new CustomEvent('falala', { detail: { yo: 123 } }))
-        }}>Falala-Yo</button>
+            //
+        }}>Walk Action</button>
     </>
 }
